89c2add64b204540cd6ea526f1dc08652618a002
botokash <botokash@gmail.com>
2022-03-31 08:00:11 +0000

    Устранил замечания
    1. Понимает многоцифровые числа
    2. Понимает дробные числа
    3. Очистка пробелов на входе
    4. Добавлена обработка исключений

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ operators = {
 }
 
 
-def check_unary(token):
-  if token.isdigit() or \
-    token == ')':
-    return False
+def check_lexeme(char):
+  if char.isdigit() or char == '.':
+    return 'digit'
+  elif char == '(':
+    return 'open_bracket'
+  elif char == ')':
+    return 'close_bracket'
+  elif char in operators:
+    return 'operator'
   else:
-    return True
+    return 'error'
 
 
 def grade(operator):
@@ -24,53 +29,107 @@ def eval(operator, x, y):
 
 
 def sorting_yard(exp):
-  result = []
-  stack = []
-  sign = ''
-  unary = True
-  for token in exp:
-    if token.isdigit():
-      result.append(sign + token)
-      sign = ''
-    elif token == '(':
-      stack.append(token)
-    elif token == ')':
-      while stack:
-        operator = stack.pop()
+  result_stack = []
+  oper_stack = []
+  last_type = ''
+
+  for lexeme in exp:
+    type = check_lexeme(lexeme)
+    
+    if type == 'error':
+      raise ValueError('Symbol error: ' + lexeme)
+    
+    if type == 'digit':
+      if last_type == 'digit':
+        result_stack[-1]['value'] += lexeme
+      else:
+        result_stack.append(
+          {
+            'type': type,
+            'value': lexeme,
+          }
+        )
+
+    elif type == 'open_bracket':
+      oper_stack.append(lexeme)
+
+    elif type == 'close_bracket':
+      while oper_stack:
+        operator = oper_stack.pop()
         if operator == '(':
           break
         else:
-          result.append(operator)
-    elif token in operators:
-      if unary and \
-        (token == '-' or token == '+'):
-        sign = token
+          result_stack.append(
+            {
+              'type': 'operator',
+              'value': operator,
+            }
+          )
+
+    elif type == 'operator':
+      if (lexeme == '-' or lexeme == '+') \
+        and (not result_stack \
+            or (last_type == 'operator' \
+                or last_type == 'open_bracket') \
+         ):
+        type = 'digit'
+        result_stack.append(
+          {
+            'type': 'digit',
+            'value': lexeme,
+          }
+        )
       else:
-        if stack and \
-          (stack[-1] in operators) and \
-          (grade(token) <= grade(stack[-1])):
-          result.append(stack.pop())
-        stack.append(token)
-        sign = ''
-    unary = check_unary(token)
-  while stack:
-    result.append(stack.pop())
-  return result
+        if oper_stack \
+          and (oper_stack[-1] in operators) \
+          and (grade(lexeme) <= grade(oper_stack[-1])):
+          result_stack.append(
+            {
+              'type': 'operator',
+              'value': oper_stack.pop(),
+            }
+          )
+        oper_stack.append(lexeme)
+    
+    last_type = type
+
+  while oper_stack:
+    result_stack.append(
+      {
+        'type': 'operator',
+        'value': oper_stack.pop(),
+      }
+    )
+
+  return result_stack
 
 
-def calc(polish):
-  stack = []
-  for token in polish:
-    if token in operators:
-      y, x = stack.pop(), stack.pop()
-      stack.append(eval(token, x, y))
+def polish_repr(data):
+  tokens = []
+  for el in data:
+    tokens.append(el['value'])
+  return ' '.join(tokens)
+
+def calc(stack):
+  buff_stack = []
+  for token in stack:
+    if token['type'] == 'operator':
+      if len(buff_stack) < 2:
+        raise ValueError('Formula incorrect')
+      y, x = buff_stack.pop(), buff_stack.pop()
+      buff_stack.append(eval(token['value'], x, y))
     else:
-      stack.append(int(token))
-  return stack[0]
+      buff_stack.append(float(token['value']))
+  return buff_stack[0]
 
 
 exp = input()
-polish = sorting_yard(exp)
-print(''.join(polish))
-result = calc(polish)
-print(result)
+exp = exp.replace(' ', '')
+
+try:
+  exp_stack = sorting_yard(exp)
+  print(polish_repr(exp_stack))
+  result = calc(exp_stack)
+  print(result)
+except ValueError as err:
+  print(err)
